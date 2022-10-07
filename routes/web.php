8bcea8526a449c09c9d6f132e88b4b8fc9a54155
Kevin <69651491+Kevin-Ponciano@users.noreply.github.com>
2022-10-07 19:22:07 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\TarefasController;
 use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/',[TasksController::class,'index']);
-Route::post('/store',[TasksController::class,'store']);
-Route::post('/update/{task}',[TasksController::class,'update']);
-Route::post('/update_order',[TasksController::class,'update_order']);
-Route::post('/destroy/{id}',[TasksController::class,'destroy']);
+//Route::get('/',[TasksController::class,'index']);
+//Route::post('/store',[TasksController::class,'store']);
+//Route::post('/update/{task}',[TasksController::class,'update']);
+//Route::post('/update_order',[TasksController::class,'update_order']);
+//Route::post('/destroy/{id}',[TasksController::class,'destroy']);
 
+Route::controller(TarefasController::class)->group(function (){
+    Route::get('/','index');
+    Route::post('/store','store');
+    Route::post('/update/{task}','update');
+    Route::post('/update_order','update_order');
+    Route::post('/destroy/{id}','destroy');
+});
