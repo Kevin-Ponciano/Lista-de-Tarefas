896b6f56e319dcc309f409322d7f8ba3ff5a31ce
Kevin <69651491+Kevin-Ponciano@users.noreply.github.com>
2022-10-07 21:16:01 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
 use App\Http\Controllers\TarefasController;
-use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
-
-
-//Route::get('/',[TasksController::class,'index']);
-//Route::post('/store',[TasksController::class,'store']);
-//Route::post('/update/{task}',[TasksController::class,'update']);
-//Route::post('/update_order',[TasksController::class,'update_order']);
-//Route::post('/destroy/{id}',[TasksController::class,'destroy']);
 
 Route::controller(TarefasController::class)->group(function (){
     Route::get('/','index');
